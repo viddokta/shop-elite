108f2404eef1a4338ab465d431c2f18fcf191378
Viddxml_ <davidokta56@gmail.com>
2024-01-08 00:12:34 +0700

    Application

---
+++ inc/hooks/tgm.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Recommended plugins
+ *
+ * @package shop-elite
+ */
+if ( ! function_exists( 'shop_elite_recommended_plugins' ) ) :
+	/**
+	 * Recommend plugins.
+	 *
+	 * @since 1.0.0
+	 */
+	function shop_elite_recommended_plugins() {
+		$plugins = array(
+            array(
+                'name'     => esc_html__( 'Woocommerce', 'shop-elite' ),
+                'slug'     => 'woocommerce',
+                'required' => false,
+            ),
+            array(
+                'name'     => esc_html__( 'YITH WooCommerce Wishlist', 'shop-elite' ),
+                'slug'     => 'yith-woocommerce-wishlist',
+                'required' => false,
+            )
+		);
+		tgmpa( $plugins );
+	}
+endif;
+add_action( 'tgmpa_register', 'shop_elite_recommended_plugins' );
